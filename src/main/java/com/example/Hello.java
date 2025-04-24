c586017d3491192d490cc810c11bca825fd22598
Cassio Fernandes <cassio.fernandes@sap.com>
2025-04-24 16:54:37 -0300

    feat: adding another greeting and farewell

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -2,8 +2,17 @@ package com.example;
 
 public class Hello {
     public String greet(String name) {
-        String greeting = "Hello, " + name;
-        return greeting;
+        return "Hello, " + name;
+    }
+
+    public String greetWithRambomMessage(String name) {
+        String[] messages = {"Hello", "Hi", "Greetings", "Salutations"};
+        int randomIndex = (int) (Math.random() * messages.length);
+        return messages[randomIndex] + ", " + name;
+    }
+
+    public String farewell(String name) {
+        String message = "Goodbye, " + name;
+        return message;
     }
 }
-// comentário de teste 2
