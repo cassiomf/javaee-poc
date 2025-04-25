8fe3054a90ae19bedd0d46c661d9a8fbeabd4ab9
Cassio Fernandes <cassio.fernandes@sap.com>
2025-04-25 15:07:04 -0300

    feat: adding random

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -4,4 +4,16 @@ public class Hello {
     public String greet(String name) {
         return "Hello, " + name;
     }
+
+    public String greetWithRandomString(String name) {
+        String[] array = {
+            "Hello, " + name,
+            "Hi, " + name,
+            "Greetings, " + name,
+            "Salutations, " + name,
+            "Howdy, " + name
+        };
+        int randomIndex = (int) (Math.random() * array.length);
+        return array[randomIndex];
+    }
 }
