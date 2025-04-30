b0a66c55e296dd96d0616cd0613d29dde63de1f6
Cassio Fernandes <cassio.fernandes@sap.com>
2025-04-30 10:51:05 -0300

    feat: adding languages

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -4,4 +4,18 @@ public class Hello {
     public String greet(String name) {
         return "Hello, " + name;
     }
+
+    public String greetWithLanguages(String name, String language) {
+        if ("Spanish".equals(language)) {
+            return "Hola, " + name;
+        } else if ("French".equals(language)) {
+            return "Bonjour, " + name;
+        } else if ("French".equals(language)) {
+            return "Bonjour, " + name;
+        } else if ("Portuguese".equals(language)) {
+            return "Olá, " + name;
+        } else {
+            return greet(name);
+        }
+    }
 }
