9dac5ce699c85eeb0ae937bc8a85428b4020e2e1
Cassio Fernandes <cassio.fernandes@sap.com>
2025-04-30 10:11:36 -0300

    feat: adding multiple languages

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -4,4 +4,8 @@ public class Hello {
     public String greet(String name) {
         return "Hello, " + name;
     }
+
+    public String greetWithFunnyMessage(String name) {
+        return "Hello, " + name + "! Did you know that the average person walks the equivalent of three times around the world in a lifetime?";
+    }
 }
